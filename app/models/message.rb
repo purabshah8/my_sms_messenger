363e4ec3f7c8298a33a71acdd1ceabfc5ec0f095
Purab Shah <purabshah8@gmail.com>
2025-04-17 19:58:07 -0400

    validate phone number format

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,9 @@ class Message
   field :body, type: String
   field :sent_at, type: Time
 
-  validates :to, presence: true
-  validates :from, presence: true
+  validates :to, :from, presence: true, format: {
+    with: /\A\+\d{1,3}\d{10}\z/,
+    message: "invalid format, ensure phone number has country code and no spaces or dashes"
+  }
   validates :body, presence: true
 end
