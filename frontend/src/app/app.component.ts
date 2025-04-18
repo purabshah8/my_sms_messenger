f1aba9cca19bbd8264d229770d20e0d5b38cf73b
Purab Shah <purabshah8@gmail.com>
2025-04-17 21:06:31 -0400

    add message list component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageFormComponent } from './components/message-form/message-form.component'
+import { MessageListComponent } from './components/message-list/message-list.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MessageFormComponent],
+  imports: [RouterOutlet, MessageFormComponent, MessageListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
